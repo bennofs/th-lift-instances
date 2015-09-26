1e630456c99812eff51745b7c616342d1aed1dc5
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-09-26 19:20:28 +0200

    add install-tools script

---
+++ travis/install-tools.hs
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -d $HOME/tools/bin ]; then
+    mkdir -p $HOME/tools
+    cd $HOME/tools
+    cabal-$CABALVER sandbox init
+    cabal-$CABALVER install hlint packunused -j
+    ln -s $PWD/.cabal-sandbox/bin $PWD/bin
+    cd $TRAVIS_BUILD_DIR
+fi
