93e7f56237e5d2108701ab0243cb21ec19636cae
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2020-04-01 14:35:08 +0200

    travis(): test cabal v2 nix-style builds

--- travis/install.sh
+++ travis/install.sh
@@ -9,11 +9,15 @@ begin_steps
 # cabal's solver.
 CABAL_FULL_VERSION=$(cabal --version | grep -P -o 'using version \K[^ ]+')
 step "Computing install plan" << EOF
-  cabal install --dry -v --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"} > installplan.txt
-
-  # -v prints things some things we are not interested in, like commands containing random temporary path.
-  # The install plan starts after the line "Resolving dependencies...", so we will just delete everything up to that.
-  sed -i -e '1,/^Resolving dependencies/d' installplan.txt
+  if [ -n "$V2" ]; then
+    cabal v2-build --dry -v --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"} > installplan.txt
+    sed -i -e '1,/^In order, the following would be built:/d' installplan.txt
+  else
+    cabal install --dry -v --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"} > installplan.txt
+    # -v prints things some things we are not interested in, like commands containing random temporary path.
+    # The install plan starts after the line "Resolving dependencies...", so we will just delete everything up to that.
+    sed -i -e '1,/^Resolving dependencies/d' installplan.txt
+  fi
 
   # We need to install a Cabal library that matches the version cabal-install was compiled against
   echo "Cabal $CABAL_FULL_VERSION" >> installplan.txt
@@ -26,19 +30,23 @@ if diff -u installplan.txt $HOME/.cabsnap/installplan.txt; then
   step "Restoring build cache" << EOF
     rm -rf .ghc
     cp -a $HOME/.cabsnap/ghc $HOME/.ghc
-    cp -a $HOME/.cabsnap/lib $HOME/.cabsnap/share $HOME/.cabsnap/bin $HOME/.cabal
+    cp -a $HOME/.cabsnap/lib $HOME/.cabsnap/share $HOME/.cabsnap/bin $HOME/.cabsnap/store $HOME/.cabal
 EOF
 else
   step "Clearing build cache and installing dependencies" << EOF
     rm -rf $HOME/.cabsnap
-    mkdir -p $HOME/.ghc $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin
-    cabal install --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"}
-    cabal install Cabal --constraint "Cabal==$CABAL_FULL_VERSION"
+    mkdir -p $HOME/.ghc $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin $HOME/.cabal/store
+    if [ -n "$V2" ]; then
+      cabal v2-build --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"}
+    else
+      cabal install --only-dependencies --enable-tests --enable-benchmarks ${ALLOW_NEWER:+--allow-newer="$ALLOW_NEWER"}
+      cabal install Cabal --constraint "Cabal==$CABAL_FULL_VERSION"
+    fi
 EOF
   step "Saving build cache" << EOF
     mkdir $HOME/.cabsnap
     cp -a $HOME/.ghc $HOME/.cabsnap/ghc
-    cp -a $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin installplan.txt $HOME/.cabsnap/
+    cp -a $HOME/.cabal/lib $HOME/.cabal/share $HOME/.cabal/bin $HOME/.cabal/store installplan.txt $HOME/.cabsnap/
 EOF
 fi
 
@@ -56,10 +64,7 @@ EOF
     step "Installing tools" << EOF
       rm -rf $HOME/tools
       mkdir -p $HOME/tools
-      cd $HOME/tools
-      cabal sandbox init
-      cabal install $TOOLS --constraint "Cabal==$CABAL_FULL_VERSION"
-      ln -s $HOME/tools/.cabal-sandbox/bin $HOME/tools/bin
+      cabal --store-dir $HOME/tools v2-install --installdir $HOME/tools/bin $TOOLS --constraint "Cabal==$CABAL_FULL_VERSION"
       rm -f $HOME/tools/bin/{happy,ghc,alex,cabal}
 EOF
     cp toolversions.txt $HOME/tools
